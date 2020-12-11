cc35414e3e9271d26a54c5a73001bd9b1525e1c9
Arthur Quemard <arthur@arthurs-macbook-pro.home>
2020-12-11 10:40:32 +0100

    Implement client server socket solutions - Implement the workshop solution - Implement a smarter solution with multithreading

---
+++ SimpleSolution/Client/Client.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Client
+{
+    class Client
+    {
+        static void Main(string[] args)
+        {
+            Socket socket = SeConnecter();
+            EcouterReseau(socket);
+        }
+
+        private static Socket SeConnecter()
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            IPEndPoint ipep = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 9050);
+
+            Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                server.Connect(ipep);
+
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("Unable to connect to server.");
+
+                Console.WriteLine(e.ToString());
+
+
+            }
+            return server;
+        }
+        private static void EcouterReseau(Socket server)
+        {
+            //waiting for serveur message
+            byte[] data = new byte[1024];
+            int recv = server.Receive(data);
+
+            string input, stringData;
+
+            stringData = Encoding.UTF8.GetString(data, 0, recv);
+
+            Console.WriteLine(stringData);
+
+            while (true)
+            {
+                // waiting for user entry
+                Console.Write(": ");
+                input = Console.ReadLine();
+
+                // send user entry in socket (client message)
+                Console.WriteLine("Client: " + input);
+                server.Send(Encoding.UTF8.GetBytes(input));
+
+                //catch exit string to disconnect
+                if (input == "exit") {
+                    Deconnecter(server);
+                    break;
+                }
+
+                //waiting for serveur message
+                data = new byte[1024];
+                recv = server.Receive(data);
+
+                // Print serveur received message
+                stringData = Encoding.UTF8.GetString(data, 0, recv);
+                Console.WriteLine("Server: " + stringData);
+            }
+
+        }
+        private static void Deconnecter(Socket socket)
+        {
+            Console.WriteLine("Disconnecting from server...");
+            socket.Shutdown(SocketShutdown.Both);
+            socket.Close();
+
+            Console.WriteLine("Disconnected!");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SimpleSolution/Serveur/Server.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.CompilerServices;
+
+namespace Serveur
+{
+    class Server
+    {
+        // static Socket client; //static Socket newsock;
+        static IPEndPoint clientep;
+
+        static void Main(string[] args)
+        {
+            Socket socket = SeConnecter();
+            Socket client = AccepterConnection(socket);
+            EcouterReseau(client);
+            Deconnecter(socket);
+        }
+        private static Socket SeConnecter()
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            IPEndPoint ipep = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 9050);
+            Socket newsock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            newsock.Bind(ipep);
+            newsock.Listen(10);
+
+            Console.WriteLine("Serveur disponible à l'écoute ...");
+
+            return newsock;
+        }
+        private static Socket AccepterConnection(Socket socket)
+        {
+            Socket client = socket.Accept();
+            clientep = (IPEndPoint)client.RemoteEndPoint;
+
+            Console.WriteLine("Connecté avec l'adresse {0} et port {1}", clientep.Address, clientep.Port);
+
+            return client;
+        }
+
+        private static void EcouterReseau(Socket client)
+        {
+            string input;
+            int recv;
+
+            string welcome = "Bienvenue sur le serveur ...";
+
+            // send message in socket
+            byte[] data = new byte[1024];
+            data = Encoding.UTF8.GetBytes(welcome);
+            client.Send(data, data.Length, SocketFlags.None);
+
+
+            while (true)
+            {
+
+                // waiting for client message
+                recv = client.Receive(data);
+
+                // catch exit message from client
+                if (Encoding.UTF8.GetString(data, 0, recv) == "exit")
+                    break;
+
+                // Print client received message
+                Console.WriteLine("Client: " + Encoding.UTF8.GetString(data, 0, recv));
+
+                // waiting for user entry
+                Console.Write(": ");
+                input = Console.ReadLine();
+
+                // Send user entry in socket (server message)
+                Console.WriteLine("Server : " + input);
+                client.Send(Encoding.UTF8.GetBytes(input));
+            }
+        }
+        private static void Deconnecter(Socket socket)
+        {
+            Console.WriteLine("Disconnected from {0}", clientep.Address);
+            socket.Close();
+
+            Console.ReadLine();
+        }
+    }
+}
